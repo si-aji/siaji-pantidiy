8b5a25d4a08fe5a25d0e9d82426356b6f22272ab
SIAJI <si.aji03@gmail.com>
2020-01-10 10:28:15 +0700

    Add Role

---
+++ app/Http/Controllers/Dashboard/RoleController.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use DB;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::all();
+        return view('content.dashboard.roles.index', compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $options = [
+            'pages' => ['list', 'create', 'edit', 'delete'],
+            'post' => ['list', 'create', 'edit', 'delete'],
+            'category' => ['list', 'create', 'edit'],
+            'keyword' => ['list', 'create', 'edit'],
+            'panti_list' => ['list', 'create', 'edit'],
+            'panti_liputan' => ['list', 'create', 'edit'],
+            'location_provinsi' => ['list', 'create', 'edit'],
+            'location_kabupaten' => ['list', 'create', 'edit'],
+            'location_kecamatan' => ['list', 'create', 'edit'],
+        ];
+
+        return view('content.dashboard.roles.create', compact('options'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'roles_name' => ['required', 'string', 'max:191', 'unique:roles,name'],
+            'permission.*' => ['nullable']
+        ]);
+
+        $role = new Role;
+        $role->name = $request->roles_name;
+        $role->save();
+
+        foreach($request->permission as $value){
+            $check = Permission::where('name', $value)->first();
+            $store = null;
+
+            if(empty($check)){
+                $store = Permission::create([
+                    'name' => $value
+                ]);
+            }
+        }
+
+        // Assign Permission
+        $role->syncPermissions($request->permission);
+
+        return redirect()->route('dashboard.roles-setting.index')->with([
+            'action' => 'Store',
+            'message' => 'Role successfully added'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $role = Role::findOrFail($id);
+        $old_options = Permission::join('role_has_permissions', 'permissions.id', '=', 'role_has_permissions.permission_id')
+            ->where('role_has_permissions.role_id', $id)
+            ->get()->pluck('name');
+        return view('content.dashboard.roles.show', compact('role', 'old_options'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $role = Role::findOrFail($id);
+        $old_options = Permission::join('role_has_permissions', 'permissions.id', '=', 'role_has_permissions.permission_id')
+            ->where('role_has_permissions.role_id', $id)
+            ->get()->pluck('name');
+
+        $options = [
+            'pages' => ['list', 'create', 'edit', 'delete'],
+            'post' => ['list', 'create', 'edit', 'delete'],
+            'category' => ['list', 'create', 'edit'],
+            'keyword' => ['list', 'create', 'edit'],
+            'panti_list' => ['list', 'create', 'edit'],
+            'panti_liputan' => ['list', 'create', 'edit'],
+            'location_provinsi' => ['list', 'create', 'edit'],
+            'location_kabupaten' => ['list', 'create', 'edit'],
+            'location_kecamatan' => ['list', 'create', 'edit'],
+        ];
+
+
+        return view('content.dashboard.roles.edit', compact('role', 'options', 'old_options'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'roles_name' => ['required', 'string', 'max:191', 'unique:roles,name,'.$id],
+            'permission.*' => ['nullable']
+        ]);
+
+        $role = Role::findOrFail($id);
+        $role->name = $request->roles_name;
+        $role->save();
+
+        foreach($request->permission as $value){
+            $check = Permission::where('name', $value)->first();
+            $store = null;
+
+            if(empty($check)){
+                $store = Permission::create([
+                    'name' => $value
+                ]);
+            }
+        }
+
+        // Assign Permission
+        $role->syncPermissions($request->permission);
+
+        return redirect()->route('dashboard.roles-setting.index')->with([
+            'action' => 'Update',
+            'message' => 'Role successfully update'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/content/dashboard/roles/create.blade.php
@@ -0,0 +1,195 @@
+@extends('layouts.dashboard', [
+    'wsecond_title' => 'Roles Setting - Create',
+    'menu' => 'roles',
+    'sub_menu' => null,
+    'alert' => [
+        'action' => Session::get('action') ?? null,
+        'message' => Session::get('message') ?? null
+    ],
+    'content_header' => [
+        'title' => 'Roles Setting',
+        'desc' => null,
+        'breadcrumb' => [
+            [
+                'url' => route('dashboard.index'),
+                'text' => 'Dashboard',
+                'active' => false
+            ], [
+                'url' => route('dashboard.roles-setting.index'),
+                'text' => 'Roles Setting',
+                'active' => false
+            ], [
+                'url' => '#',
+                'text' => 'Create',
+                'active' => true
+            ]
+        ]
+    ]
+])
+
+@section('content')
+<form class="card" method="POST" action="{{ route('dashboard.roles-setting.store') }}">
+    @csrf
+    
+    <div class="card-header card-primary card-outline">
+        <h1 class="card-title">Add new Roles</h1>
+        <div class="card-tools">
+            <a href="{{ route('dashboard.roles-setting.index') }}" class="btn btn-secondary btn-sm">
+                <i class="fas fa-chevron-circle-left"></i> Back
+            </a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="form-group">
+            <label for="field-roles_name">Name</label>
+            <input type="text" name="roles_name" id="field-roles_name" class="form-control @error('roles_name') is-invalid @enderror" placeholder="Roles Name" value="{{ old('roles_name') }}">
+            @error('roles_name')
+            <div class='invalid-feedback'>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <table class="table table-hover table-bordered mb-3">
+            <thead>
+                <tr>
+                    <th>Field</th>
+                    <th>
+                        Access
+                        <div class="btn-group float-right">
+                            <button type="button" class="btn btn-sm btn-warning" id="allow_all" onclick="checkAll();" style="display:none">Check All</button>
+                            <button type="button" class="btn btn-sm btn-info" id="notallow_all" onclick="uncheckAll();" style="display:none">Un-Check All</button>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($options as $key => $item)
+                    <tr>
+                        <td>{{ ucwords(str_replace('_', ' - ', $key)) }}</td>
+                        <td>
+                            <div class="form-row">
+                                @foreach($item as $value)
+                                <div class="col-12 col-lg-3">
+                                    <div class="custom-control custom-checkbox d-block d-lg-inline">
+                                        <input class="custom-control-input" type="checkbox" name="permission[]" value="{{ $key.'-'.$value }}" id="{{ $key.'-'.$value }}" {!! $value != 'list' ? 'disabled' : "onchange=listCheck('".$key."')" !!}>
+                                        <label for="{{ $key.'-'.$value }}" class="custom-control-label">{{ ucwords($value) }}</label>
+                                    </div>
+                                </div>
+                                @endforeach
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="form-group row">
+            <div class="offset-sm-2 col-sm-10 text-right text-md-right">
+                <button type="reset" id="btn-reset" class="btn btn-sm btn-danger">Reset</button>
+                <button type="submit" class="btn btn-sm btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
+</form>
+@endsection
+
+@section('inline_js')
+<script>
+    var key_list = [
+        @foreach($options as $key => $value)
+        '{{ $key }}',
+        @endforeach
+    ]
+
+    @if(old('permission'))
+    var key_old = [];
+    @foreach(old('permission') as $permission)
+    key_old.push("{{ $permission }}");
+    @endforeach
+    @endif
+
+    $(document).ready(function(){
+        key_list.forEach(function(obj){
+            @if(old('permission'))
+                if(key_old.includes(obj+'-list')){
+                    $("#"+obj+'-list').prop('checked', true);
+                }
+            @endif
+
+            listCheck(obj);
+        });
+        checkboxCheck();
+    });
+
+    function listCheck(permission){
+        console.log("Check Permission is running...");
+
+        if($("#"+permission+"-list").prop('checked') === true){
+            $("#"+permission+'-create').attr('disabled', false);
+            $("#"+permission+'-edit').attr('disabled', false);
+            $("#"+permission+'-delete').attr('disabled', false);
+
+            @if(old('permission'))
+            if(key_old.includes(permission+'-create')){
+                $("#"+permission+'-create').prop('checked', true);
+            } else {
+                $("#"+permission+'-create').prop('checked', false);
+            }
+
+            if(key_old.includes(permission+'-edit')){
+                $("#"+permission+'-edit').prop('checked', true);
+            } else {
+                $("#"+permission+'-edit').prop('checked', false);
+            }
+
+            if(key_old.includes(permission+'-delete')){
+                $("#"+permission+'-delete').prop('checked', true);
+            } else {
+                $("#"+permission+'-delete').prop('checked', false);
+            }
+            @endif
+        } else {
+            $("#"+permission+'-create').prop('checked', false).attr('disabled', true);
+            $("#"+permission+'-edit').prop('checked', false).attr('disabled', true);
+            $("#"+permission+'-delete').prop('checked', false).attr('disabled', true);
+        }
+    }
+
+    $("input[name='permission[]']").change(function(){
+        checkboxCheck();
+    });
+
+    function checkboxCheck(){
+        let avail_option = $("input[name='permission[]']").length;
+        let selected_option = $("input[name='permission[]']:checked").length;
+
+        // console.log("Avail Option "+avail_option);
+        // console.log("Selected Option "+selected_option);
+        if(selected_option >= avail_option){
+            $("#allow_all").attr('disabled', true).hide();
+            $("#notallow_all").attr('disabled', false).show();
+        } else {
+            $("#allow_all").attr('disabled', false).show();
+            $("#notallow_all").attr('disabled', true).hide();
+        }
+    }
+    function checkAll(){
+        $("input[name='permission[]']").prop('disabled', false).prop('checked', true);
+        checkboxCheck();
+    }
+    function uncheckAll(){
+        $("input[name='permission[]']").prop('checked', false);
+        key_list.forEach(function(obj){
+            listCheck(obj);
+        });
+        checkboxCheck();
+    }
+
+    $("#btn-reset").click(function(){
+        key_list.forEach(function(obj){
+            $("#"+obj+"-list").prop('checked', false);
+            listCheck(obj);
+        });
+        checkboxCheck();
+    });
+</script>
+@endsection
